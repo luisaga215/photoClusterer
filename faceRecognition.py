db63eb96270d0e4f018ad8e0d296ac86e3b836a3
luisaga215 <luis.aga215@gmail.com>
2022-09-23 16:21:32 -0500

    Using CNN model and resizing target image with opencv

--- faceRecognition.py
+++ faceRecognition.py
@@ -1,42 +1,52 @@
 import face_recognition
 import os
 import shutil
+import cv2
 
 
-def clusterPhotosByFace(myPhotoPath,targetPhotosDir,resultsDir):
-    ##Get all the target photos file names in a list
-    photosList = os.listdir(targetPhotosDir)
+def clusterPhotosByFace(myPhotoPath,targetDir,resultsDir):
+    ## Get all the target photos file names in a list
+    photosList = os.listdir(targetDir)
 
 
-    ##Get face encodings of my photo
+    ## Get face encodings of my photo
     myImage = face_recognition.load_image_file(myPhotoPath)
     myFaceEncoding = face_recognition.face_encodings(myImage)[0]
 
-    ##Iterate through the photosList
+    faceLocations = []
+    faceEncodings = []
+    ## Iterate through the photosList
     for photo in photosList:
 
-        ##Get photo face locations and face encodings
-        targetPhoto = face_recognition.load_image_file(targetPhotosDir + '/' + photo)
-        faceLocations = face_recognition.face_locations(targetPhoto)
-        faceEncodings = face_recognition.face_encodings(targetPhoto, faceLocations)
+        ## Use opencv to read the image and resize it to 1/4 size for faster processing
+        targetPhoto = cv2.imread(targetDir + '/' + photo)
+        targetPhotoResized = cv2.resize(targetPhoto, (0, 0), fx=0.25, fy=0.25)
 
-        ##Iterate through face encodings to compare to myFaceEncoding
+        ## Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        targetPhotoResized_RGB = targetPhotoResized[:, :, ::-1]
+        
+        ## Get photo face locations and face encodings using CNN model
+        faceLocations = face_recognition.face_locations(targetPhotoResized_RGB,1,'cnn')
+        faceEncodings = face_recognition.face_encodings(targetPhotoResized_RGB, faceLocations)
+        
+        ## Iterate through face encodings to compare to myFaceEncoding
         for targetEncoding in faceEncodings:
-            ##By default compare_faces needs a list as a parameter, for testing purposes we are using the same image twice as a list
+            
+            ## By default compare_faces needs a list as a parameter, for testing purposes we are using the same image twice as a list
             matches = face_recognition.compare_faces([myFaceEncoding,myFaceEncoding], targetEncoding)
 
-            ##If True then move the photo from root to results directory and break the loop (since we are only looking for a single face, one encoding match is enough)
+            ## If True then move the photo from root to results directory and break the loop (since we are only looking for a single face, one encoding match is enough)
             if True in matches:
-                shutil.move(targetPhotosDir+'/'+photo, resultsDir+'/'+photo)
+                shutil.move(targetDir+'/'+photo, resultsDir+'/'+photo)
                 break
-
+        
     print("Finished moving photos")
 
 
 ##Photos paths
 myPhotoPath = "./mypicture/me.jpg"
-targetPhotosDir = "./photos"
+targetDir = "./photos"
 resultsDir = "./results"
 
 ##Make sure the resultsDir folder exists before running the script
-clusterPhotosByFace(myPhotoPath,targetPhotosDir,resultsDir)
+clusterPhotosByFace(myPhotoPath,targetDir,resultsDir)
